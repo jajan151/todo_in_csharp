0224cee1b578f5367f438dd4dc5e0d386e24ea65
jajan151 <jajan151@seznam.cz>
2023-08-13 03:47:23 +0200

    all functions working

--- Program.cs
+++ Program.cs
@@ -2,10 +2,13 @@
 
 class TestClass
 {
+    static int whichIndex;
+    static int index;
     static bool choiceCheck;
     static string _todoName;
     static string _todoDesription;
-    static Dictionary<string, string> _todos = new();
+    static List<string> todos = new List<string>();
+    static List<string> descriptions = new List<string>();
 
     static void Main(string[] args)
     {
@@ -39,11 +42,16 @@ class TestClass
                         showTodos();
                         break;
                     case 3:
+                        editTodo();
                         break;
                     case 4:
+                        completeTodo();
                         break;
                     case 5:
                         exit();
+                        break;
+                    default:
+                        showMenu();
                         break;
                 }
             }
@@ -51,62 +59,130 @@ class TestClass
 
         static void createNewTodo()
         {
-            bool dalsi = true;
+            bool next = true;
             do
             {
                 Console.WriteLine("Add name: \n");
                 _todoName = Console.ReadLine();
                 Console.WriteLine("Add description: \n");
                 _todoDesription = Console.ReadLine();
-                _todos.Add(_todoName, _todoDesription);
+                todos.Add(_todoName);
+                descriptions.Add(_todoDesription);
+
                 Console.WriteLine("Want add another todo? Choose yes or no: ");
                 string answer = Console.ReadLine();
-
-                if (answer == "no")
+                switch (answer)
                 {
-                    dalsi = false;
-                    showMenu();
-                }
-                else
-                {
-                    Console.WriteLine("Wrong choice...back to menu\n");
-                    showMenu();
-                }
-            } while (dalsi);
-        }
-        
-        static void showTodos()
-        {
-            foreach (KeyValuePair<string, string> pair in _todos)
-            {
-                Console.WriteLine("Todo: \n {0}\n  - {1}\n", pair.Key, pair.Value);
-                Console.WriteLine("Want you go back to menu or exit application? Write menu or exit...");
-                var choice = Console.ReadLine();
-                switch (choice)
-                {
-                    case "menu":
+                    case "yes":
+                        continue;
+                    case "no":
+                        next = false;
                         showMenu();
                         break;
-                    case "exit":
-                        exit();
+                    default:
+                        Console.WriteLine("Wrong choice...back to menu!");
+                        showMenu();
                         break;
                 }
-            }
+            } 
+            while (next);
+        }
+
+        static void showTodos()
+        { 
+            enumerate();
+            escapeFromLoops();
         }
 
         static void editTodo()
         {
+            string todoOrDescription;
+            index = 1;
 
+            enumerate();
+
+            Console.WriteLine("Which todo do you want to update? Write a number: \n");
+            whichIndex = int.Parse(Console.ReadLine());
+            whichIndex -= 1;
+
+            if (whichIndex <= index)
+            {
+                Console.WriteLine("Want you update name or description? Write 't' or 'd'");
+                todoOrDescription = Console.ReadLine();
+                if (todoOrDescription == "t")
+                {
+                    Console.WriteLine("Write new name of your todo: \n");
+                    string updatedTodo = Console.ReadLine();
+                    todos[whichIndex] = updatedTodo;
+                }
+                else if (todoOrDescription == "d")
+                {
+                    Console.WriteLine("Write new name of your desription: \n");
+                    string updatedDescription = Console.ReadLine();
+                    descriptions[whichIndex] = updatedDescription;
+                }
+            }
+            else
+            {
+                Console.WriteLine("This todo doesnt exist, try it again...");
+                editTodo();
+            }
+
+            Console.WriteLine("Done, now you are redirect back to menu...");
+            showMenu();
         }
 
         static void completeTodo()
         {
+            index = 1;
+            enumerate();
+            Console.WriteLine("Which todo do you want to complete? Write a number: \n");
+            whichIndex = int.Parse(Console.ReadLine());
+            whichIndex -= 1;
 
+            if (whichIndex <= index)
+            {
+                todos.RemoveAt(whichIndex);
+                descriptions.RemoveAt(whichIndex);
+            }
+            else
+            {
+                showMenu();
+            }
+
+            escapeFromLoops();
         }
 
         static void exit()
         {
             Environment.Exit(0);
+        }
+
+        static void escapeFromLoops()
+        {
+            Console.WriteLine("Want you go back to menu or exit application? Write 'menu' or 'exit'...");
+            var choice = Console.ReadLine();
+            switch (choice)
+            {
+                case "menu":
+                    showMenu();
+                    break;
+                case "exit":
+                    exit();
+                    break;
+                default:
+                    Console.WriteLine("Wrong choice, try it again!");
+                    break;
+            }
+        }
+
+        static void enumerate()
+        {
+            foreach (var todoPair in todos.Zip(descriptions, (todo, desc) => new { Todo = todo, Description = desc }))
+            {
+                Console.WriteLine("{0}. {1}\n   - {2}", index, todoPair.Todo, todoPair.Description);
+                index++;
+            }
         }
     }
 }
